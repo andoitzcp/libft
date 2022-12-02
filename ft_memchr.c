78f4eb21b5872dad7b8afbf1de98879c1460e278
andoitzcp <andoitzcp@gmail.com>
2022-12-03 00:33:49 +0100

    ft_memchr check

--- ft_memchr.c
+++ ft_memchr.c
@@ -6,23 +6,20 @@
 /*   By: acampo-p <acampo-p@student.42urduliz.com>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/30 15:08:05 by acampo-p          #+#    #+#             */
-/*   Updated: 2022/11/30 17:02:32 by acampo-p         ###   ########.fr       */
+/*   Updated: 2022/12/03 00:32:48 by acampo-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stddef.h>
+#include "libft.h"
 
 void	*ft_memchr(const void *s, int c, size_t n)
 {
-	unsigned char *cs;
-	unsigned char cc;
+	unsigned char	*cs;
 
 	cs = (unsigned char *) s;
-	cc = (unsigned char) c;
-
 	while (n-- > 0)
 	{
-		if (*cs == cc)
+		if (*cs == c)
 			return ((void *)cs);
 		cs++;
 	}
