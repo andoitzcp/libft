70bf8b387772ed49509f8d56d396101538a9fe01
andoitzcp <andoitzcp@gmail.com>
2022-12-03 17:10:19 +0100

    ft_strlcpy check

---
+++ ft_strlcpy.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: acampo-p <acampo-p@student.42urduliz.com>  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/03 15:01:44 by acampo-p          #+#    #+#             */
+/*   Updated: 2022/12/03 16:49:43 by acampo-p         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+size_t	ft_strlcpy(char *dst, const char *src, size_t size)
+{
+	int	index;
+
+	index = 0;
+	while (src[index] != '\0' && index < size - 1)
+	{
+		dst[index] = src[index];
+		index++;
+	}
+	dst[index] = '\0';
+	return (ft_strlen(src));
+}
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: acampo-p <acampo-p@student.42urduliz.com>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/02 13:56:07 by acampo-p          #+#    #+#             */
-/*   Updated: 2022/12/03 14:10:44 by acampo-p         ###   ########.fr       */
+/*   Updated: 2022/12/03 16:18:35 by acampo-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,3 +32,5 @@ int		ft_memcmp(const void *s1, const void *s2, size_t n);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
 void	*ft_memmove(void *dest, const void *src, size_t n);
 int		ft_atoi(const char *nptr);
+size_t	ft_strlcpy(char *dst, const char *src, size_t size);
+size_t	ft_strlcat(char *dst, const char *src, size_t size);
