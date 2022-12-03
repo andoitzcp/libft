5e36abf0334ddc279045afa22d685dbc22858f69
andoitzcp <andoitzcp@gmail.com>
2022-12-03 17:31:20 +0100

    ft_strlcat check

---
+++ ft_strlcat.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: acampo-p <acampo-p@student.42urduliz.com>  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/03 17:11:03 by acampo-p          #+#    #+#             */
+/*   Updated: 2022/12/03 17:28:45 by acampo-p         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+size_t	ft_strlcat(char *dst, const char *src, size_t size)
+{
+	size_t	len;
+	size_t	index;
+
+	len = ft_strlen(dst);
+	dst += len;
+	len += ft_strlen(src);
+	index = 0;
+	while (src[index] != '\0' && index < size - 1)
+	{
+		dst[index] = src[index];
+		index++;
+	}
+	return (len);
+}
