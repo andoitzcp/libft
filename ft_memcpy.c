c5347bdde716c31019fd402f358b5aa2dd3499ac
andoitzcp <andoitzcp@gmail.com>
2022-12-03 01:22:01 +0100

    ft_memcpy norminette check

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: acampo-p <acampo-p@student.42urduliz.com>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/29 14:35:02 by acampo-p          #+#    #+#             */
-/*   Updated: 2022/12/03 01:16:16 by acampo-p         ###   ########.fr       */
+/*   Updated: 2022/12/03 01:21:31 by acampo-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,17 +14,17 @@
 
 void	*ft_memcpy(void *dest, const void *src, size_t n)
 {
-		char *cdest;
-		char *csrc;
-		size_t	index;
+	char	*cdest;
+	char	*csrc;
+	size_t	index;
 
-		cdest = (char *)dest;
-		csrc = (char *)src;
-		index = 0;
-		while (index < n)
-		{
-			cdest[index] = csrc[index];
-			index++;
-		}
-		return (dest);
+	cdest = (char *)dest;
+	csrc = (char *)src;
+	index = 0;
+	while (index < n)
+	{
+		cdest[index] = csrc[index];
+		index++;
+	}
+	return (dest);
 }
